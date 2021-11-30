19cabd71ab4b28b211af17eda82c1550fa2a5dd8
Joe Laurole <Josue.laurole@gmail.com>
2021-11-30 11:46:04 -0500

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -8,8 +8,21 @@ def import():
 
 app = Flask(__name__)
 
-@app/route('/func', methods=['GET', 'POST'])
+@app.route('/func', methods=['GET',])
 def func():
     data = request.json
     return jsonify(data)
     
+@app.route('/manage_file', methods=['POST'])
+def func2():
+        if payload = request.get_json()
+        else: status error
+        return
+
+def func3():
+        if payload['action'] == 'read':
+                return flask.jsonify 200
+        else:
+                return flask.jsonify 500
+        
+        
